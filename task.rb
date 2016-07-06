06c75c9833cacf9528603f521f91ef8c9c2b0417
Anatolii <bass_1304@ukr.net>
2016-07-06 19:28:36 +0200

    NEW notepad

--- task.rb
+++ task.rb
@@ -20,4 +20,16 @@ class Task < Post
     deadline = "Крайний срок: #{@due_date}"
     return [deadline, @text, time_string]
   end
+
+  def to_db_hash
+    return super.merge( {
+        'text' => @text,
+        'due_date' => @due_date.to_s
+                        } )
+  end
+
+  def load_data(data_hash)
+    super(data_hash)
+    @due_date = Date.parse(data_hash['due_date'])
+  end
 end
